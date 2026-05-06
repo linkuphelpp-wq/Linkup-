a4d0850ea6dba231a7546e63aecc5b4104a307d1
Muhannad Barbud <Linkup.helpp@gmail.com>
2026-05-06 22:15:34 +0300

    إصلاح حد حجم ملف PWA

--- vite.config.js
+++ vite.config.js
@@ -42,6 +42,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
       }
     })
